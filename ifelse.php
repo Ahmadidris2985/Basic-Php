391b8b1098de32f797e54d462797b946ac21883a
Ahmadidris2985 <38835722+Ahmadidris2985@users.noreply.github.com>
2018-05-18 15:58:26 +0300

    Add files via upload

---
+++ ifelse.php
@@ -0,0 +1,29 @@
+<?php
+//Next is if and else condition.
+/*
+ sa paggamit ng if else. Dapat alam nyo gamitin ang mga logical operator na AND OR NOT. Halimbawa:
+*/
+
+$mid = 72;
+$fin = 71;
+$ave = ($mid + $fin) /2;
+//Kinuha lng ung averaage ng mid at fin at nilagay sa ave(midterm at finals) Kayo n bahala if may iba kyong formula para mkuha ung average.
+
+if($ave>74){
+	echo "Passed";		
+			}
+else{
+	echo"Failed";		
+	}			
+//Passed ang lumbas. Ave = 83.5, which is mas mataas sa 74. Kaya pumasok sya sa if at ineexcute ang script n echo.Pero pag ang value ng ave ay mababa sa 74. Halimbawa:ave is 71.5, so failed ang lumbas. Pumasok sya sa else. Dumaan muna sya sa if at nagtnong if ang value ba ng ave ay mataas sa 74.. Dahil false ang sagot sa if, pumunta sya sa else.
+//Ganunlang ang nasic if else ng php... If you want n subukan ang mga sample natin, pde ko sya iupload para madownload nyo at matry. Bigay ko nlng ung link. 
+//Yun lang at salamat. (^__^) 
+
+
+
+
+
+
+
+
+?>
